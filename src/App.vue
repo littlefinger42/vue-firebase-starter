1c366e0e83cafbd43affa2e03ecf4f6873f33918
littlefinger42 <lauriemail@protonmail.com>
2017-12-18 19:15:15 +0100

    Fixed bugs in app.vue

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
           <v-list-tile-action><v-icon>person</v-icon></v-list-tile-action>
           <v-list-tile-content><v-list-tile-title>Login</v-list-tile-title></v-list-tile-content>
         </v-list-tile>
-        <v-list-tile @click="" v-show="user" to="profile">
+        <v-list-tile @click="" v-show="user" to="profile" ref="userButton">
           <v-list-tile-action><v-icon>person</v-icon></v-list-tile-action>
           <v-list-tile-content><v-list-tile-title>Profile</v-list-tile-title></v-list-tile-content>
         </v-list-tile>
